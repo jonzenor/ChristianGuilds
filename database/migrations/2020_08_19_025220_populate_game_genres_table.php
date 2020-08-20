6f6e865430f55c1c531e32b4bab032a0218bda9d
Talemire <60450916+Talemire@users.noreply.github.com>
2020-08-19 23:49:41 -0400

    Update 2020_08_19_025220_populate_game_genres_table.php

--- database/migrations/2020_08_19_025220_populate_game_genres_table.php
+++ database/migrations/2020_08_19_025220_populate_game_genres_table.php
@@ -41,6 +41,22 @@ class PopulateGameGenresTable extends Migration
         $genres[] = array('short_name' => 'MMORPG', 'name' => 'Massively Multiplayer Online Role Playing Game');
         $genres[] = array('short_name' => 'FPS', 'name' => 'First Person Shooter');
         $genres[] = array('short_name' => 'Sandbox', 'name' => 'Sandbox');
+        $genres[] = array('short_name' => 'MMOFPS', 'name' => 'Massively Multiplayer Online First Person Shooter');
+        $genres[] = array('short_name' => 'RTS', 'name' => 'Real-time Strategy');
+        $genres[] = array('short_name' => 'MMORTS', 'name' => 'Massively Multiplayer Online Real-time Strategy');
+        $genres[] = array('short_name' => 'Survival', 'name' => 'Survival');
+        $genres[] = array('short_name' => 'BR', 'name' => 'Battle Royale');
+        $genres[] = array('short_name' => 'Simulation', 'name' => 'Simulation');
+        $genres[] = array('short_name' => 'Sports', 'name' => 'Sports');
+        $genres[] = array('short_name' => 'CCG', 'name' => 'Collectable Card Game');
+        $genres[] = array('short_name' => 'RPG', 'name' => 'Role-playing Game');
+        $genres[] = array('short_name' => 'JRPG', 'name' => 'Japanese Role-playing Game');
+        $genres[] = array('short_name' => 'VR', 'name' => 'Virtual Reality');
+        $genres[] = array('short_name' => 'RL', 'name' => 'Roguelike');
+        $genres[] = array('short_name' => 'MOBA', 'name' => 'Multiplayer Online Battle Arena');
+        $genres[] = array('short_name' => 'TBS', 'name' => 'Turn-based Strategy');
+        $genres[] = array('short_name' => 'Party', 'name' => 'Party');
+        $genres[] = array('short_name' => 'Educational', 'name' => 'Educational');
 
         return $genres;
     }
