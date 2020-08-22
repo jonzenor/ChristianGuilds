a593ca48085102915fb9732973e8e611c97bc977
Jon Zenor <JonZenor@gmail.com>
2020-08-21 22:16:00 -0600

    Closes #148 Corrected minor lang bug.

--- resources/lang/en/game.php
+++ resources/lang/en/game.php
@@ -4,18 +4,18 @@ return [
 
     'count' => 'Total Games: :count',
     'stats' => 'Game DB Stats',
-    'list'  => 'Games List',
+    'list'  => 'Game List',
     'id'    => 'ID',
     'name'  => 'Name',
 
-    'genre' => 'Genre',
-    'genre_count' => 'Total Genres: :count',
+    'genre'         => 'Genre',
+    'genre_count'   => 'Total Genres: :count',
     'genre_list'    => 'Game Genre List',
     'genre_id'      => 'ID',
     'genre_name'    => 'Name',
 
-    'add'       => 'Add a Game',
-    'details'   => 'Game Details',
+    'add'               => 'Add a Game',
+    'details'           => 'Game Details',
     'add_success'       => 'Game Added Successfully!',
     'update_success'    => 'Game Updated Successfully!',
     'manage'            => 'Manage',
